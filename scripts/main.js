ed62a725b9dc7c8ed0cc1b08cfb481d727a0e8b8
Pedro Martin <pataruco@gmail.com>
2019-02-09 17:32:25 +0000

    Add instructions

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,13 @@
 // Element
+// Get footer
 var footer = document.querySelector('footer');
 
 // Event
+// Add Event to listen a click
 footer.addEventListener('click', toggleBlackAndWhite);
 
 // Execution
 function toggleBlackAndWhite() {
+  // Get body and toogle class black-and-white
   document.body.classList.toggle('black-and-white');
 }
